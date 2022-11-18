c1e8886c5026261e4a2c959bb9f0689a626fe565
Aaron Erb <erbnlegend@gmail.com>
2022-11-18 16:49:01 -0600

    Initial dist subtree commit

--- src/requests/weatherIcons.js
+++
@@ -1,8 +0,0 @@
-async function weatherIcons (weatherData) {
-  const response = await fetch(`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`, { credentials: 'omit' })
-  const urlObj = response
-  const url = urlObj.url
-  return url
-}
-
-export { weatherIcons }
