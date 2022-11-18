b020c7aa6d3e6a981d4222e2d78b01a6dce8466d
Aaron Erb <erbnlegend@gmail.com>
2022-11-18 16:46:57 -0600

    https images

--- src/requests/weatherIcons.js
+++ src/requests/weatherIcons.js
@@ -1,5 +1,5 @@
 async function weatherIcons (weatherData) {
-  const response = await fetch(`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`, { credentials: 'omit' })
+  const response = await fetch(`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`, { credentials: 'omit' })
   const urlObj = response
   const url = urlObj.url
   return url
