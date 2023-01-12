2dcc447bdc7c848bc90d27b43d8f1784f5028c31
Aaron <erbnlegend@gmail.com>
2023-01-12 16:02:01 -0600

    main refactoring start

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,74 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
 
-module.exports = {
-  entry: {
-    bundle: './src/main.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist'
-  },
-  optimization: {
-    runtimeChunk: 'single'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'imgs/[name][ext]'
+module.exports = (env, argv) => {
+  const ENV = argv.mode || 'development'
+  return {
+    entry: {
+      bundle: './src/index.js'
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, ENV === 'development' ? 'dist' : 'dist/projects/weatherApp'),
+      publicPath: '/'
+    },
+    mode: ENV,
+    devtool: ENV === 'production' ? undefined : 'inline-source-map',
+    devServer: {
+      historyApiFallback: true
+    },
+    optimization: {
+      minimize: ENV === 'production',
+      runtimeChunk: 'single'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/i,
+          use: ['babel-loader']
+        },
+        {
+          test: /.(p?css|postcss)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: ''
+              }
+            },
+            'css-loader',
+            'postcss-loader'
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'imgs/[name][ext]'
+          }
+        },
+        {
+          test: /.(html)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: '[name][ext]',
+            outputPath: './'
+          }
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource'
         }
-      },
-      {
-        test: /.(html)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: '[name][ext]',
-          outputPath: './'
-        }
-      }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles.css'
+      }),
+      new webpack.EnvironmentPlugin({
+        NODE_ENV: ENV
+      })
     ]
   }
 }
